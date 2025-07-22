3c4f28c6ee61fb10c049264d9b30f542dcc4c952
Oliver Xu <oliver.xu@pronto.net>
2025-07-22 16:05:41 +1000

    added two lines

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-'use strict';
+"use strict";
 
-const jonas = {
-    firstName: 'Jonas',
-    friends: ['Michael', 'Peter', 'Steven'],
-    job: 'teacher',
-    birthYear: 1991,
-    hasDriverLicense: false,
+// const jonas = {
+//     firstName: 'Jonas',
+//     friends: ['Michael', 'Peter', 'Steven'],
+//     job: 'teacher',
+//     birthYear: 1991,
+//     hasDriverLicense: false,
 
-    calcAge: function () {
-        this.age = 2037 - this.birthYear;
-        return this.age;
-    },
+//     calcAge: function () {
+//         this.age = 2037 - this.birthYear;
+//         return this.age;
+//     },
 
-    calcHasDriverLicense: function () {
-        return this.hasDriverLicense ? 'a' : 'no';
-    }
-}
+//     calcHasDriverLicense: function () {
+//         return this.hasDriverLicense ? 'a' : 'no';
+//     }
+// }
 
 // console.log(jonas.calcHasDriverLicense);
 
 // console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}. `);
 
-console.log(`${jonas.firstName} is a ${jonas.calcAge()}-year old ${jonas.job}, and he has ${jonas.calcHasDriverLicense()} driver's license.`);
+// console.log(`${jonas.firstName} is a ${jonas.calcAge()}-year old ${jonas.job}, and he has ${jonas.calcHasDriverLicense()} driver's license.`);
+
+const printForecast = function (temp) {
+  let strPrint = "...";
+  for (let i = 0; i < temp.length; i++) {
+    strPrint += ` ${temp[i]}°C in ${i + 1} days ...`;
+  }
+  console.log(strPrint);
+};
+
+printForecast([12, 5, -5, 0, 4]);
